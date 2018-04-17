ecd76b9a92ef87dd9db792adbf416d11e1146ed1
Taras Bukachevskyy <tarasb@sas.upenn.edu>
2018-04-16 20:36:26 -0400

    server.js: added query function and create a get data response

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-//const jwt = require('jsonwebtoken');
-//const config = require('./config');
-
 var oracledb = require('oracledb');
-oracledb.getConnection(
-  {
-    user          : 'team13',
-    password      : 'team13!!',
-    connectString : '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=finalprojectcis450.cjppovwc5n4y.us-east-2.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SID=ORCL)))'
-  }, function(err, connection) {
-    if (err) {
-      console.log(err.message);
-      return;
-    }
-    console.log('connected');
-  });
 
-// instantiate express app1
+//function which takes in a queryString and executes the query. Returns promise which will return data as array of objects.
+let query = (queryString) => {
+  oracledb.getConnection(
+    {
+      user          : 'team13',
+      password      : 'team13!!',
+      connectString : '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=finalprojectcis450.cjppovwc5n4y.us-east-2.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SID=ORCL)))'
+    })
+    .then((connection) => {
+      console.log('connected');
+      return connection.execute(queryString);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+
+// instantiate express app
 const app = express();
 
+app.get('/', function(req, res) {
+  res.send('./public/sign-in.html');
+});
+
+
+app.get('/api/data', function(req, res) {
+  query(queryString) //needs actual query
+  .then((data) => {
+    res.json(data); //returns array of js objects as returned rows of query
+  })
+  .catch((err) => {
+    res.sendStatus("500");
+    console.log(err.message);
+  })
+})
 
 // set the superSecret key in our app which we will use  to sign our jwts
 //app.set('superSecret', config.secret);
